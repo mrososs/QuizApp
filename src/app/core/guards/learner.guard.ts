5c9ed57ebbbae701d335fe614d335a125e4d6f4d
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-05-04 13:01:54 +0300

    feat (storgeService , Guards) : handle storge service and guards

---
+++ src/app/core/guards/learner.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { StorgeService } from '../services/storge.service';
+
+export const learnerGuard: CanActivateFn = (route, state) => {
+   const storage = inject(StorgeService);
+    const router = inject(Router);
+    const role = storage.getUserRole();
+  
+    if (role === 'Learner') {
+      return true;
+    }
+    router.navigate(['/login']);
+    return false;
+};
