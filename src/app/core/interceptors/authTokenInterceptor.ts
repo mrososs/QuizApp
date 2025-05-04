43dfa706b936889b3f69de755f43b60368e46652
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-05-04 13:09:32 +0300

    feat(Interceptors) : add interceptors handlers

---
+++ src/app/core/interceptors/authTokenInterceptor.ts
@@ -0,0 +1,26 @@
+// src/app/core/interceptors/auth-token.interceptor.ts
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { StorgeService } from '../services/storge.service';
+
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const storage = inject(StorgeService);
+  const token = storage.getAccessToken();
+
+  // Skip requests to /auth endpoints
+  if (req.url.includes('/auth')) {
+    return next(req);
+  }
+
+  // Apply token only to /instructor or /learner APIs
+  if ((req.url.includes('/instructor') || req.url.includes('/learner')) && token) {
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(authReq);
+  }
+
+  return next(req);
+};
