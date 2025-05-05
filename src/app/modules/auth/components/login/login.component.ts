50b5ecc225c2fb112aa33c1b367dc05d0604a152
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-05-05 03:44:26 +0300

    feat ( login component and shared style ) : create login component and matformfield style

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { SharedLayoutComponent } from '../shared-layout/shared-layout.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [SharedLayoutComponent,CommonModule,ReactiveFormsModule],
+  imports: [
+    SharedLayoutComponent,
+    MatIconModule,
+    MatInputModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  passwordVisible = false;
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
