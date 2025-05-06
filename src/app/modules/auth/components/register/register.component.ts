ad75e692c3af5c61410ae70159e4110a14c40457
emanbadr <amonaelshafey252@gmail.com>
2025-05-07 00:58:58 +0300

    chore :( ui && ts )register component

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,32 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedLayoutComponent } from '../shared-layout/shared-layout.component';
 import { CommonModule } from '@angular/common';
-import {
-  FormGroup, NonNullableFormBuilder, ReactiveFormsModule,Validators,} from '@angular/forms';
+import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule,Validators,} from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { RegisterData, RegisterParams } from '../../models/register';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-register',
   standalone: true,
    imports: [
-      MatIconModule,
-      CommonModule,
-      ReactiveFormsModule,
-    ],
+    MatIconModule,
+    CommonModule,
+    ReactiveFormsModule,
+    SharedLayoutComponent ,
+    MatSelectModule ,
+    MatFormFieldModule ,FormsModule
+
+],
     providers: [AuthService],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent {
+  role: string[] = ['Student' , 'instructor' ]
+  passwordVisible:boolean = false
 
-  constructor(private _AuthService:AuthService){}
+  registerForm = new FormGroup({
+    first_name:new FormControl('', [Validators.required ]) ,
+    last_name:new FormControl('', [Validators.required ]) ,
+    email:new FormControl('', [Validators.required , Validators.email]) ,
+    password:new FormControl('', [Validators.required ]) ,
+    role:new FormControl('', [Validators.required ]) ,
+  })
 
-  ngOnInit(): void {}
-  
-  register( registerData : RegisterParams):void{
+
+  constructor(private _AuthService:AuthService ,
+     private _Router: Router
+  ){}
+
+  onSubmit(): void {
+    if(this.registerForm.valid){
+      // console.log(this.registerForm.value);
+      this.register( this.registerForm.value)
+    }
+  }
+  register( registerData : any ):void{
     this._AuthService.register(registerData).subscribe({
       next:(res)=> {
         console.log(res);
@@ -34,7 +58,13 @@ export class RegisterComponent implements OnInit{
       error:(err)=> {
         console.log(err);
       },
+      complete:()=> {
+        this._Router.navigate(['/auth/login'])
+      },
     })
   }
 
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
