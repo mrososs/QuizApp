b28bed3e0d8c1a54c00f25d1d1cbaae8c977a6e4
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-05-11 17:05:52 +0300

    Handle

---
+++ src/app/modules/dashboard/components/shared/shared-input/shared-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-input',
+  standalone: false,
+  templateUrl: './shared-input.component.html',
+  styleUrl: './shared-input.component.scss'
+})
+export class SharedInputComponent {
+
+}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+
+import { SharedInputComponent } from '../dashboard/components/shared/shared-input/shared-input.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule,MatButtonModule,MatIconModule],
-  exports: [CommonModule, MatButtonModule, MatIconModule],
+  declarations: [
+    SharedInputComponent
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
+  exports: [CommonModule, MatButtonModule, MatIconModule,SharedInputComponent],
 })
 export class SharedModule {}
