6fbecea2bf0c1d3a96530dfac5f8831b00b85c14
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-05-20 19:14:02 +0300

    feat: Quiz Dashboard

--- src/app/modules/dashboard/modules/instructor/modules/quizzes/quizzes-routing.module.ts
+++ src/app/modules/dashboard/modules/instructor/modules/quizzes/quizzes-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizDashboardComponent } from './components/quiz-dashboard/quiz-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component : QuizDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/dashboard/modules/instructor/modules/quizzes/quizzes.module.ts
+++ src/app/modules/dashboard/modules/instructor/modules/quizzes/quizzes.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QuizzesRoutingModule } from './quizzes-routing.module';
+import { QuizDashboardComponent } from './components/quiz-dashboard/quiz-dashboard.component';
+import { SharedModule } from '../../../../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    
+    QuizDashboardComponent
   ],
   imports: [
     CommonModule,
-    QuizzesRoutingModule
+    QuizzesRoutingModule,
+    SharedModule
   ]
 })
 export class QuizzesModule { }
